fd62b22e278c0bc26b52adf6d05b1a625b28db42
cm-collins <dev-collins@outlook.com>
2026-04-15 12:26:39 +0300

    Add clickable functionality to BookListItem and implement LoadingIndicator component

--- app/src/main/java/com/speechify/composeuichallenge/ui/screens/booklist/components/BookListItem.kt
+++ app/src/main/java/com/speechify/composeuichallenge/ui/screens/booklist/components/BookListItem.kt
@@ -1,8 +1,9 @@
 package com.speechify.composeuichallenge.ui.screens.booklist.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,7 +15,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,13 +23,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.speechify.composeuichallenge.R
 import com.speechify.composeuichallenge.data.Book
-import com.speechify.composeuichallenge.ui.theme.ComposeUIChallengeTheme
-
 @Composable
 fun BookListItem(
     book: Book,
@@ -50,6 +47,7 @@ fun BookListItem(
                 .fillMaxHeight()
                 .aspectRatio(3f / 4f)
                 .clip(RoundedCornerShape(12.dp))
+                .clickable(enabled = true , onClick = { onDetailsClick(book.id) })
         )
 
         Spacer(modifier = Modifier.width(16.dp))
